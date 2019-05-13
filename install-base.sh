5f4a644e1ea0fc02a1629ba23eeff0d149f8ca20
m.vanhoof <m.vanhoof@youwe.nl>
2019-05-13 16:56:46 +0200

    initial commit

---
+++ install-base.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+user=$(id -nu)
+uid=$(id -u)
+gid=$(id -g)
+
+#Removing containers and images
+docker-compose stop --volumes --remove-orphans --rmi all
+
+#Removing var directories
+bash -c "rm --recursive --force vendor/ var/ web/var/ web/bundles/"
+
+#Warming up var directories
+bash -c "mkdir var/"
+bash -c "cp --recursive app/Resources/docker/mysql/log var/mysql/"
+bash -c "cp --recursive app/Resources/docker/php/log var/php/"
+bash -c "cp --recursive app/Resources/docker/apache/log var/apache/"
+
+#Building images
+docker-compose build --build-arg user=${user} --build-arg uid=${uid} --build-arg gid=${gid} "mysql" "redis" "php" "apache"
+
+#Starting containers
+docker-compose up --no-build --remove-orphans --detach "mysql" "redis" "php" "apache"
+
+#Installing composer packages
+docker-compose exec php bash -c "COMPOSER_MEMORY_LIMIT=-1 composer install --no-suggest --no-interaction --dev -vvv"
+
+#Installing pimcore
+docker-compose exec php bash -c "vendor/bin/pimcore-install --env=dev --ignore-existing-config --no-interaction -vvv"
+
+#Installing assets
+docker-compose exec php bash -c "bin/console assets:install --env=dev --symlink -vvv"
+
+#clearing symfony cache
+docker-compose exec php bash -c "bin/console cache:clear --env=dev --no-interaction --no-warmup -vvv"
+
+#clearing pimcore cache
+docker-compose exec php bash -c "bin/console pimcore:cache:clear --env=dev --no-interaction -vvv"
---
+++ install.sh
@@ -0,0 +1,48 @@
+git clone https://github.com/JeroenVanDerLaan/pimcore-boilerplate.git "5.8"
+
+cp ./install-base.sh 5.8/app/Resources/docker/install.sh
+cp ./services.yml 5.8/app/Resources/docker/services.yml
+
+cd 5.8
+./app/Resources/docker/install.sh
+cd ..
+
+declare -a versions
+versions[0]=5.4.0
+versions[1]=5.4.1
+versions[2]=5.4.2
+versions[3]=5.4.3
+versions[4]=5.4.4
+versions[5]=5.5.0
+versions[6]=5.5.1
+versions[7]=5.5.2
+versions[8]=5.5.3
+versions[9]=5.5.4
+versions[10]=5.6.0
+versions[11]=5.6.1
+versions[12]=5.6.2
+versions[13]=5.6.3
+versions[14]=5.6.4
+versions[15]=5.6.5
+versions[16]=5.6.6
+versions[17]=5.7.0
+versions[18]=5.7.1
+versions[19]=5.7.2
+versions[20]=5.7.3
+versions[21]=5.8.0
+versions[22]=5.8.1
+versions[23]=5.8.2
+
+for i in "${versions[@]}"; do
+    mkdir -p $i;
+done
+for i in "${versions[@]}"; do
+    cp -r 5.8 $i
+    cp ./install-base.sh ./$i/app/Resources/docker/install.sh
+    PIMCORE_VERSION=$i
+    echo "PIMCORE_VERSION=$i" >> ./$i/.env
+    cp ./composer-base.json ./$i/composer.json
+    cd $i
+    ./app/Resources/docker/install.sh
+    cd ..
+done
