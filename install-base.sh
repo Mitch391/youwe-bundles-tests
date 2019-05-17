110f69287a77896a90e55ee290d4407492c43c66
Michel <m.vanhoof@youwe.nl>
2019-05-17 12:17:44 +0200

    add echos

--- install-base.sh
+++ install-base.sh
@@ -20,19 +20,23 @@ bash -c "cp -r app/Resources/docker/apache/log var/apache/"
 docker-compose build --build-arg user=${user} --build-arg uid=${uid} --build-arg gid=${gid} "mysql" "redis" "php" "apache"
 
 #Starting containers
-docker-compose up --no-build --remove-orphans --detach "mysql" "redis" "php" "apache"
+docker-compose up --no-build --remove-orphans --detach "php"
+docker-compose up --no-build --remove-orphans --detach "mysql" "redis" "apache"
 
-#Installing composer packages
+echo "Waiting for comtainers to boot..."
+sleep 5
+
+echo Installing composer packages
 docker-compose exec php bash -c "COMPOSER_MEMORY_LIMIT=-1 composer install --no-suggest --no-interaction --dev -vvv"
 
-#Installing pimcore
+echo Installing pimcore
 docker-compose exec php bash -c "vendor/bin/pimcore-install --env=dev --ignore-existing-config --no-interaction -vvv"
 
-#Installing assets
+echo Installing assets
 docker-compose exec php bash -c "bin/console assets:install --env=dev --symlink -vvv"
 
-#clearing symfony cache
+echo clearing symfony cache
 docker-compose exec php bash -c "bin/console cache:clear --env=dev --no-interaction --no-warmup -vvv"
 
-#clearing pimcore cache
+echo clearing pimcore cache
 docker-compose exec php bash -c "bin/console pimcore:cache:clear --env=dev --no-interaction -vvv"
